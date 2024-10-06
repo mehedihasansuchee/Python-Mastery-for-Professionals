940b17d3b725eb88a7695dc3daf26ea6e3648b42
mehedihasansuchee <mehedihasansuchee@outlook.com>
2024-10-06 14:59:35 +0600

    kivy

--- 0.1.frontend/kivy/practice/mehedi/bismillah.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-class BismillahApp(App):
-    def build(self):
-        return Label(text="Bismillah")
-    
-if __name__ == "__main__":
-    BismillahApp().run()
--- 0.1.frontend/kivy/practice/mehedi/modularization/module1/data/questions.py
+++
@@ -1,6 +0,0 @@
-questions = [
-    {"question": "What is the capital of France?", "options": ["Paris", "London", "Berlin"], "answer": "Paris"},
-    {"question": "What is the capital of Spain?", "options": ["Madrid", "Barcelona", "Valencia"], "answer": "Madrid"},
-    {"question": "What is the capital of Italy?", "options": ["Rome", "Milan", "Naples"], "answer": "Rome"},
-    # Add more questions as needed
-]
